8ede539ebe49069a9aa779bc7b5a2a412061f7d7
Todd Wolfson <todd@twolfson.com>
2012-11-15 01:13:06 -0800

    Finished converting over diagonal algorithm

--- test/layout_test.js
+++ test/layout_test.js
@@ -82,5 +82,26 @@ exports['layout'] = {
         test.equal(result.items[2].x, 20, 'Third item is 20 pixels from the left');
 
     test.done();
+  },
+  'diagonal': function (test) {
+    test.expect(7);
+
+    // A top-down layout
+    var topDown = layout('diagonal');
+      // with multiple items
+      topDown.addItem({'height': 20, 'width': 10, 'meta': 'medium'}); // 20 x 10
+      topDown.addItem({'height': 10, 'width': 10, 'meta': 'small'});  // 30 x 20
+      topDown.addItem({'height': 50, 'width': 40, 'meta': 'large'});  // 80 x 60
+        // organizes them in a top-down manner
+        var result = topDown['export']();
+        test.equal(result.height, 80, 'Result has a height of 80');
+        test.equal(result.width, 60, 'Result has a width of 60');
+        test.equal(result.items.length, 3, 'Result has 3 items');
+        test.equal(result.items[1].y, 10, 'Second item is 10 pixels from the top');
+        test.equal(result.items[1].x, 10, 'Second item is 10 pixels from the left');
+        test.equal(result.items[2].y, 30, 'Third item is 30 pixels from the top');
+        test.equal(result.items[2].x, 20, 'Third item is 20 pixels from the left');
+
+    test.done();
   }
 };

fc395028b57435a15ce5c07bccc08d4763a4fabd
Todd Wolfson <todd@twolfson.com>
2012-11-15 01:07:16 -0800

    Added left to right test

--- test/layout_test.js
+++ test/layout_test.js
@@ -44,7 +44,7 @@ exports['layout'] = {
     test.done();
   },
   'top-down': function (test) {
-    test.expect(5);
+    test.expect(6);
 
     // A top-down layout
     var topDown = layout('top-down');
@@ -57,8 +57,29 @@ exports['layout'] = {
         test.equal(result.height, 80, 'Result has a height of 80');
         test.equal(result.width, 40, 'Result has a width of 40');
         test.equal(result.items.length, 3, 'Result has 3 items');
+        test.equal(result.items[1].x, 0, 'Second item is 0 pixels from the left');
         test.equal(result.items[1].y, 10, 'Second item is 10 pixels from the top');
         test.equal(result.items[2].y, 30, 'Third item is 30 pixels from the top');
+
+    test.done();
+  },
+  'left-right': function (test) {
+    test.expect(6);
+
+    // A top-down layout
+    var topDown = layout('left-right');
+      // with multiple items
+      topDown.addItem({'height': 20, 'width': 10, 'meta': 'medium'}); // 20 x 10
+      topDown.addItem({'height': 10, 'width': 10, 'meta': 'small'});  // 20 x 20
+      topDown.addItem({'height': 50, 'width': 40, 'meta': 'large'});  // 50 x 60
+        // organizes them in a top-down manner
+        var result = topDown['export']();
+        test.equal(result.height, 50, 'Result has a height of 50');
+        test.equal(result.width, 60, 'Result has a width of 60');
+        test.equal(result.items.length, 3, 'Result has 3 items');
+        test.equal(result.items[1].y, 0, 'Second item is 0 pixels from the top');
+        test.equal(result.items[1].x, 10, 'Second item is 10 pixels from the left');
+        test.equal(result.items[2].x, 20, 'Third item is 20 pixels from the left');
 
     test.done();
   }

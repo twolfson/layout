859b981659d7a9bd95bf00fa0f74019e7e7df671
Todd Wolfson <todd@twolfson.com>
2014-12-14 06:22:13 -0600

    Deciding to use squares for sprites

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,31 +4,31 @@ module.exports = function (grunt) {
   grunt.initConfig({
     sprite: {
       'top-down': {
-        src: ['docs/small.png', 'docs/wide.png', 'docs/tall.png'],
+        src: ['docs/sprite.*.png'],
         destImg: 'docs/top-down.png',
         destCSS: 'docs/top-down.css',
         algorithm: 'top-down'
       },
       'left-right': {
-        src: ['docs/small.png', 'docs/wide.png', 'docs/tall.png'],
+        src: ['docs/sprite.*.png'],
         destImg: 'docs/left-right.png',
         destCSS: 'docs/left-right.css',
         algorithm: 'left-right'
       },
       'diagonal': {
-        src: ['docs/small.png', 'docs/wide.png', 'docs/tall.png'],
+        src: ['docs/sprite.*.png'],
         destImg: 'docs/diagonal.png',
         destCSS: 'docs/diagonal.css',
         algorithm: 'diagonal'
       },
       'alt-diagonal': {
-        src: ['docs/small.png', 'docs/wide.png', 'docs/tall.png'],
+        src: ['docs/sprite.*.png'],
         destImg: 'docs/alt-diagonal.png',
         destCSS: 'docs/alt-diagonal.css',
         algorithm: 'alt-diagonal'
       },
       'binary-tree': {
-        src: ['docs/small.png', 'docs/wide.png', 'docs/tall.png'],
+        src: ['docs/sprite.*.png'],
         destImg: 'docs/binary-tree.png',
         destCSS: 'docs/binary-tree.css',
         algorithm: 'binary-tree'
